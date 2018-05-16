5082e729a87481d48ed952bcb942d823b7a209d2
Asparlose <asparlose@gmail.com>
2018-05-17 05:44:55 +0900

    :star2: Asparlose.DisposableObject

---
+++ DisposableObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace Asparlose
+{
+    public abstract class DisposableObject : IDisposable
+    {
+        protected event EventHandler Disposed;
+        protected bool IsDisposed { get; }
+
+        int disposed;
+
+        protected virtual void ReleaseUnmanagedResources() { }
+        protected virtual void ReleaseManagedResources() { }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (Interlocked.Exchange(ref disposed, 1) == 0)
+            {
+                if (disposing)
+                    ReleaseManagedResources();
+
+                ReleaseUnmanagedResources();
+
+                Disposed?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        ~DisposableObject()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
